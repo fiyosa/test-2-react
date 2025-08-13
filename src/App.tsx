2ce14b44df9fadde40c8b9930aee410d5619078a
fiyosa <fiyosa97@gmail.com>
2025-08-13 10:29:31 +0700

    add: env test 18

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
-  console.log('test 8');
+  console.log('test 9');
   
   console.log({
     APP_NAME: import.meta.env.VITE_APP_NAME ?? 'Default App Name',
